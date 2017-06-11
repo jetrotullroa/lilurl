0b65202f3dfeee382f6ab634d2f80fe3c4a61e1a
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-06-11 23:58:30 +0800

    readded display_new_short_link that was accidentally deleted

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -26,6 +26,10 @@ class LinksController < ApplicationController
 
   private
 
+  def display_new_short_link(short_link)
+    request.port.blank? ? request.host : "#{request.host}:#{request.port}/#{short_link}"
+  end
+
   def link_params
     params.require(:link).permit(:original_link)
   end
