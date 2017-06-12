c49187b0cc8f518a05b65e597a945cc6727a0f2b
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-06-12 17:43:42 +0800

    edit default devise user route and fix name of controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :links, only: [:index, :new, :create]
 
   get '/:short_link' => 'links#show'
