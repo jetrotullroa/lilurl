f55eff626ff8f927e2745176a8f0b89f8939d96b
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-06-14 10:55:00 +0800

    add use creator of link and time to link index

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -4,4 +4,17 @@ module LinksHelper
     request.local? ? "#{request.host}:#{request.port}/#{short_link}" : "#{request.host}/#{short_link}"
   end
 
+  def get_creators_name(link)
+    if link.user_id?
+      user = User.find(link.user_id)
+      "Created by #{user.username}"
+    else
+      "Created by an anonymous"
+    end
+  end
+
+  def link_date(link)
+    time_ago_in_words(link.created_at)
+  end
+
 end
